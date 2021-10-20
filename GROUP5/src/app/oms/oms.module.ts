89b7b9cbc473b9874120ca34791271dad75edd89
vscalligraphia <vishusaini251999@gmail.com>
2021-10-20 15:25:29 +0530

    FIRST Commit

---
+++ GROUP5/src/app/oms/oms.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OmsRoutingModule } from './oms-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OmsRoutingModule
+  ]
+})
+export class OmsModule { }
---
+++ GROUP5/src/app/role/role.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoleRoutingModule } from './role-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RoleRoutingModule
+  ]
+})
+export class RoleModule { }
